384163e08e27bdf763330431a30a3ab660d88cc5
Alex.M <alex.makeev.b95@gmail.com>
2022-04-11 20:55:54 +0700

    Setup project

---
+++ Points/Application/ApplicationDelegate.swift
@@ -0,0 +1,41 @@
+//
+//  Created by Alex.M on 11.04.2022.
+//
+
+import UIKit
+
+@main
+class ApplicationDelegate: UIResponder, UIApplicationDelegate {
+	var window: UIWindow?
+
+	func application(
+		_ application: UIApplication,
+		didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+	) -> Bool {
+		startRootScreen()
+		return true
+	}
+}
+
+private extension ApplicationDelegate {
+	var rootWindow: UIWindow {
+		if let window = window {
+			return window
+		} else {
+			let window = UIWindow(frame: UIScreen.main.bounds)
+			self.window = window
+			return window
+		}
+	}
+
+	func startRootScreen() {
+		let viewController = UIViewController()
+		viewController.title = "Points challenge"
+		viewController.view = UITableView()
+		let navigationController = UINavigationController(
+			rootViewController: viewController
+		)
+		rootWindow.rootViewController = navigationController
+		rootWindow.makeKeyAndVisible()
+	}
+}
